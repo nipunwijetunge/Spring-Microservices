f0014b1c93daf883df1bf89e198392f740f718ff
Nipun Wijetunge <57147260+nipunwijetunge@users.noreply.github.com>
2023-08-25 00:10:28 +0530

    security paused

---
+++ api-gateway/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.nv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
