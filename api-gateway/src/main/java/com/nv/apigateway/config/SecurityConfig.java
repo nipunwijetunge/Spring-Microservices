a15d42ed186b6d0ab66c19ff959dc0fc9d8d0ca2
Nipun Wijetunge <57147260+nipunwijetunge@users.noreply.github.com>
2023-08-27 16:44:05 +0530

    added basic auth for eureka server with custom username and password

--- api-gateway/src/main/java/com/nv/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/nv/apigateway/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.nv.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -20,7 +21,7 @@ public class SecurityConfig {
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
         return serverHttpSecurity.build();
     }
 }
